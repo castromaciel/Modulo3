5fc1269a9873695a0a30774c55ff6c310d573d42
castromaciel <castromaciel00@gmail.com>
2023-02-16 15:18:48 -0300

    feat: Add product card

--- clase03/src/components/ProductCard/ProductCard.js
+++ clase03/src/components/ProductCard/ProductCard.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import './productcard.css'
 
 const ProductCard = ({ title, id, category, description, image, price }) => {
   return (
-    <div className="card" style={{ width: '18rem' }}>
-      {/* TODO: Crear la Card con forma de producto. */}
-      ProductCard
-    </div>
+    <article className="product-card gap-3 px-3" id={id}>
+      <header className='card-header'>
+        <img src={image} alt={title} />
+      </header>
+
+      <section className='card-body d-flex flex-column gap-2'>
+        <hr />
+        <h6>{title}</h6>
+        <div>
+          <span className="badge text-bg-secondary">{category}</span>
+        </div>
+        <p>{description}</p>
+      </section>
+      
+      <footer className='card-footer pb-2'>
+        <h5>${Number(price).toFixed(2)}</h5>
+        <button className='btn btn-outline-dark'>Comprar</button>
+      </footer>
+    </article>
   )
 }
 
